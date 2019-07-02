28715b9cc96963a7a3b5ecf072c514ece7cf80ac
Gao Han <869912332@qq.com>
2019-07-02 17:13:58 +0800

    返回顶部

--- js/common.js
+++ js/common.js
@@ -1,10 +1,33 @@
-// rem换算，参数size为设计图宽度尺寸（单位为像素）
-function setRootSize(size) {
-	document.documentElement.style.fontSize = screen.width*100 / size + 'px';
+// rem/px换算，参数size为设计图宽度尺寸（单位为像素）
+(function setRootSize(size) {
+	var _doc = document.documentElement
+	function setSize() {
+		_doc.style.fontSize = _doc.clientWidth * 100 / size + 'px';
+	};
+	setSize();
+	window.addEventListener('resize', function() {
+		setSize();
+	}, false);
+})(750)
+
+// 返回顶部
+function backTop(btn) {
+	var _btn = document.getElementById(btn);
+	if (!_btn) return
+	_btn.onclick = function() {
+		var _currentScroll = window.scrollY;
+		var _offset = _currentScroll / 20;
+		function scrollToTop() {
+			var _pos = window.scrollY;
+			if ( _pos > 0 ) {
+				window.scroll(0, _pos - _offset);
+				window.requestAnimationFrame(scrollToTop);
+			}
+		};
+		scrollToTop();
+	}
 }
-window.addEventListener('resize', function() {
-	setRootSize(750);
-}, false);
+
 window.onload = function() {
-	setRootSize(750);
+	backTop('backop');
 };
