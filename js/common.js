dfcf5b1e384964395f523f107d4d0935f7549efd
Gao Han <869912332@qq.com>
2019-10-14 09:22:37 +0800

    优化返回顶部

--- js/common.js
+++ js/common.js
@@ -16,13 +16,13 @@ function backTop(btn) {
 	if (!_btn) return
 	_btn.onclick = function() {
 		var _currentScroll = window.scrollY;
-		var _offset = _currentScroll / 20;
+		var _offset = _currentScroll / 15;
 		function scrollToTop() {
 			var _pos = window.scrollY;
 			if ( _pos > 0 ) {
 				window.scroll(0, _pos - _offset);
-				setTimeout(scrollToTop, 20)
-				// window.requestAnimationFrame(scrollToTop);
+				// setTimeout(scrollToTop, 20)
+				window.requestAnimationFrame(scrollToTop);
 			}
 		};
 		scrollToTop();
